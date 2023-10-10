b6bda1c040321c36d29b30d00540224560599db7
Willian Ricardo Oliveira <willian.vancouver@gmail.com>
2023-10-10 13:19:16 -0300

    animacao

--- modelo/src/app/components/grid/grid.component.ts
+++ modelo/src/app/components/grid/grid.component.ts
@@ -1,15 +1,21 @@
+import { triggerDestaque } from '@/animations';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.scss']
+  styleUrls: ['./grid.component.scss'],
+
+  animations: [triggerDestaque]
+
 })
 export class GridComponent implements OnInit {
 
   @Input() lista: any
 
   @Input() param: any
+
+  indexTarefa = -1
 
   constructor() {}
 
